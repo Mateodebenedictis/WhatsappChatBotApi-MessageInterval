c32b23294eda098ea21fd046116ce12e1e92a5de
MateoDeBenedictis <43358221+Mateodebenedictis@users.noreply.github.com>
2022-10-25 18:34:18 -0300

    cambio de intervalo de prueba a 20 segundos

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const axios = require("axios");
 let yaSeEjecuto = false;
 
+//FUNCION REAL
+/*
 const correrCodigoAlas23 = () => {
 
-    const date = new Date();
+    let date = new Date();
+    date.setHours( date.getHours() - 3);
+
     const hour = date.getHours();
     const minute = date.getMinutes();
 
-    //if(hour === 18 && yaSeEjecuto == false){
+    if(hour === 23 && yaSeEjecuto == false){
+
+        let url = `https://api.callmebot.com/whatsapp.php?phone=5491133521081&text=Clarita+son+las+${hour + ':' + minute}+tenes+que+tomar+la+pastilla+%F0%9F%92%8A&apikey=9497027`
+
+        yaSeEjecuto = true;
+
+        axios.get(url)
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    } 
+    
+    if(hour !== 23){
+        yaSeEjecuto = false;
+        console.log('No es la hora');
+    }
+
+};
+*/
+
+//FUNCION DE PRUEBA
+const correrCodigoAlas23 = () => {
+
+    let date = new Date();
+    date.setHours( date.getHours() - 3);
+
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+
+    if(hour === 18 && yaSeEjecuto == false){
 
         let url = `https://api.callmebot.com/whatsapp.php?phone=5491133521081&text=Clarita+son+las+${date.toLocaleTimeString()}+tenes+que+tomar+la+pastilla+%F0%9F%92%8A&apikey=9497027`
 
@@ -21,7 +58,7 @@ const correrCodigoAlas23 = () => {
             console.log(error);
         });
 
-    //} 
+    } 
     
     if(hour !== 18){
         yaSeEjecuto = false;
@@ -29,6 +66,7 @@ const correrCodigoAlas23 = () => {
     }
 
 };
+
 
 //INTERVALO DE PRUEBA CADA 20 SEGUNDOS
 let intervalo = setInterval(correrCodigoAlas23, 20000);
